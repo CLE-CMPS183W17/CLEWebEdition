bca26e7d8aa9da62be76dd1275d0ac37c7da036a
Tyler Schmidt <divark@users.noreply.github.com>
2017-02-15 13:33:55 -0800

    Updated to address table add bug

--- database/course.sql
+++ database/course.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 9.6.1
 -- Dumped by pg_dump version 9.6.1
 
--- Started on 2017-02-12 22:48:59 PST
+-- Started on 2017-02-15 13:32:47 PST
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -15,6 +15,23 @@ SET standard_conforming_strings = on;
 SET check_function_bodies = false;
 SET client_min_messages = warning;
 SET row_security = off;
+
+--
+-- TOC entry 1 (class 3079 OID 12392)
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- TOC entry 2135 (class 0 OID 0)
+-- Dependencies: 1
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
 
 SET search_path = public, pg_catalog;
 
@@ -34,7 +51,9 @@ CREATE TABLE course (
     summer boolean,
     fall boolean,
     winter boolean,
-    spring boolean
+    spring boolean,
+    concurrents character varying(50),
+    prerequisites character varying
 );
 
 
@@ -56,7 +75,7 @@ CREATE SEQUENCE course_id_seq
 ALTER TABLE course_id_seq OWNER TO teststudent;
 
 --
--- TOC entry 2143 (class 0 OID 0)
+-- TOC entry 2137 (class 0 OID 0)
 -- Dependencies: 186
 -- Name: course_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: teststudent
 --
@@ -65,7 +84,7 @@ ALTER SEQUENCE course_id_seq OWNED BY course.id;
 
 
 --
--- TOC entry 2014 (class 2604 OID 16503)
+-- TOC entry 2005 (class 2604 OID 16503)
 -- Name: course id; Type: DEFAULT; Schema: public; Owner: teststudent
 --
 
@@ -73,19 +92,19 @@ ALTER TABLE ONLY course ALTER COLUMN id SET DEFAULT nextval('course_id_seq'::reg
 
 
 --
--- TOC entry 2136 (class 0 OID 16495)
+-- TOC entry 2127 (class 0 OID 16495)
 -- Dependencies: 185
 -- Data for Name: course; Type: TABLE DATA; Schema: public; Owner: teststudent
 --
 
-COPY course (id, name, units, summer, fall, winter, spring) FROM stdin;
-10	My Course	3	f	f	f	f
-11	My Other Course	3	f	f	f	f
+COPY course (id, name, units, summer, fall, winter, spring, concurrents, prerequisites) FROM stdin;
+10	My Course	3	f	f	f	f	\N	\N
+11	My Other Course	3	f	f	f	f	\N	\N
 \.
 
 
 --
--- TOC entry 2144 (class 0 OID 0)
+-- TOC entry 2138 (class 0 OID 0)
 -- Dependencies: 186
 -- Name: course_id_seq; Type: SEQUENCE SET; Schema: public; Owner: teststudent
 --
@@ -94,7 +113,7 @@ SELECT pg_catalog.setval('course_id_seq', 11, true);
 
 
 --
--- TOC entry 2016 (class 2606 OID 16505)
+-- TOC entry 2007 (class 2606 OID 16505)
 -- Name: course course_pkey; Type: CONSTRAINT; Schema: public; Owner: teststudent
 --
 
@@ -103,7 +122,7 @@ ALTER TABLE ONLY course
 
 
 --
--- TOC entry 2018 (class 2606 OID 16507)
+-- TOC entry 2009 (class 2606 OID 16507)
 -- Name: course coursename_unique; Type: CONSTRAINT; Schema: public; Owner: teststudent
 --
 
@@ -112,7 +131,7 @@ ALTER TABLE ONLY course
 
 
 --
--- TOC entry 2142 (class 0 OID 0)
+-- TOC entry 2136 (class 0 OID 0)
 -- Dependencies: 185
 -- Name: course; Type: ACL; Schema: public; Owner: teststudent
 --
@@ -120,7 +139,7 @@ ALTER TABLE ONLY course
 GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE course TO PUBLIC;
 
 
--- Completed on 2017-02-12 22:48:59 PST
+-- Completed on 2017-02-15 13:32:47 PST
 
 --
 -- PostgreSQL database dump complete
