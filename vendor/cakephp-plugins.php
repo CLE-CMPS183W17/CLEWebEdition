b9ece597c2d785417e6ede082487a410d700a24a
James Huang <jameshuanguc@gmail.com>
2017-03-01 08:59:18 -0800

    tried to add postgre

--- vendor/cakephp-plugins.php
+++
@@ -1,9 +0,0 @@
-<?php
-$baseDir = dirname(dirname(__FILE__));
-return [
-    'plugins' => [
-        'Bake' => $baseDir . '/vendor/cakephp/bake/',
-        'DebugKit' => $baseDir . '/plugins/DebugKit/',
-        'Migrations' => $baseDir . '/vendor/cakephp/migrations/'
-    ]
-];
