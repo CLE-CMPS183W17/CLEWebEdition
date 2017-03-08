8404531e9fbbfd64d1494f74b1897ac20a3ff17c
Tyler Schmidt <divark@users.noreply.github.com>
2017-03-08 13:55:43 -0800

    Units Changed to Real(Decimal)

--- database/db.sql
+++ database/db.sql
@@ -3,9 +3,9 @@
 --
 
 -- Dumped from database version 9.6.1
--- Dumped by pg_dump version 9.6.2
+-- Dumped by pg_dump version 9.6.1
 
--- Started on 2017-03-06 17:29:43
+-- Started on 2017-03-08 13:55:01 PST
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -17,7 +17,7 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- TOC entry 1 (class 3079 OID 13277)
+-- TOC entry 1 (class 3079 OID 12392)
 -- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
 --
 
@@ -25,7 +25,7 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
--- TOC entry 3043 (class 0 OID 0)
+-- TOC entry 2157 (class 0 OID 0)
 -- Dependencies: 1
 -- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
 --
@@ -40,14 +40,14 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
--- TOC entry 185 (class 1259 OID 2444088)
--- Name: course; Type: TABLE; Schema: public; Owner: dxucgijykcoqlp
+-- TOC entry 185 (class 1259 OID 16576)
+-- Name: course; Type: TABLE; Schema: public; Owner: teststudent
 --
 
 CREATE TABLE course (
     id integer NOT NULL,
     name character varying(50) NOT NULL,
-    units smallint NOT NULL,
+    units real NOT NULL,
     summer boolean,
     fall boolean,
     winter boolean,
@@ -55,11 +55,11 @@ CREATE TABLE course (
 );
 
 
-ALTER TABLE course OWNER TO dxucgijykcoqlp;
+ALTER TABLE course OWNER TO teststudent;
 
 --
--- TOC entry 190 (class 1259 OID 2444112)
--- Name: course_concurrents; Type: TABLE; Schema: public; Owner: dxucgijykcoqlp
+-- TOC entry 186 (class 1259 OID 16579)
+-- Name: course_concurrents; Type: TABLE; Schema: public; Owner: teststudent
 --
 
 CREATE TABLE course_concurrents (
@@ -69,11 +69,11 @@ CREATE TABLE course_concurrents (
 );
 
 
-ALTER TABLE course_concurrents OWNER TO dxucgijykcoqlp;
+ALTER TABLE course_concurrents OWNER TO teststudent;
 
 --
--- TOC entry 189 (class 1259 OID 2444110)
--- Name: course_concurrents_id_seq; Type: SEQUENCE; Schema: public; Owner: dxucgijykcoqlp
+-- TOC entry 187 (class 1259 OID 16582)
+-- Name: course_concurrents_id_seq; Type: SEQUENCE; Schema: public; Owner: teststudent
 --
 
 CREATE SEQUENCE course_concurrents_id_seq
@@ -84,20 +84,20 @@ CREATE SEQUENCE course_concurrents_id_seq
     CACHE 1;
 
 
-ALTER TABLE course_concurrents_id_seq OWNER TO dxucgijykcoqlp;
+ALTER TABLE course_concurrents_id_seq OWNER TO teststudent;
 
 --
--- TOC entry 3046 (class 0 OID 0)
--- Dependencies: 189
--- Name: course_concurrents_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: dxucgijykcoqlp
+-- TOC entry 2159 (class 0 OID 0)
+-- Dependencies: 187
+-- Name: course_concurrents_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: teststudent
 --
 
 ALTER SEQUENCE course_concurrents_id_seq OWNED BY course_concurrents.id;
 
 
 --
--- TOC entry 186 (class 1259 OID 2444094)
--- Name: course_id_seq; Type: SEQUENCE; Schema: public; Owner: dxucgijykcoqlp
+-- TOC entry 188 (class 1259 OID 16584)
+-- Name: course_id_seq; Type: SEQUENCE; Schema: public; Owner: teststudent
 --
 
 CREATE SEQUENCE course_id_seq
@@ -108,20 +108,20 @@ CREATE SEQUENCE course_id_seq
     CACHE 1;
 
 
-ALTER TABLE course_id_seq OWNER TO dxucgijykcoqlp;
+ALTER TABLE course_id_seq OWNER TO teststudent;
 
 --
--- TOC entry 3047 (class 0 OID 0)
--- Dependencies: 186
--- Name: course_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: dxucgijykcoqlp
+-- TOC entry 2160 (class 0 OID 0)
+-- Dependencies: 188
+-- Name: course_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: teststudent
 --
 
 ALTER SEQUENCE course_id_seq OWNED BY course.id;
 
 
 --
--- TOC entry 188 (class 1259 OID 2444103)
--- Name: course_prerequisites; Type: TABLE; Schema: public; Owner: dxucgijykcoqlp
+-- TOC entry 189 (class 1259 OID 16586)
+-- Name: course_prerequisites; Type: TABLE; Schema: public; Owner: teststudent
 --
 
 CREATE TABLE course_prerequisites (
@@ -131,11 +131,11 @@ CREATE TABLE course_prerequisites (
 );
 
 
-ALTER TABLE course_prerequisites OWNER TO dxucgijykcoqlp;
+ALTER TABLE course_prerequisites OWNER TO teststudent;
 
 --
--- TOC entry 187 (class 1259 OID 2444101)
--- Name: course_prerequisites_id_seq; Type: SEQUENCE; Schema: public; Owner: dxucgijykcoqlp
+-- TOC entry 190 (class 1259 OID 16589)
+-- Name: course_prerequisites_id_seq; Type: SEQUENCE; Schema: public; Owner: teststudent
 --
 
 CREATE SEQUENCE course_prerequisites_id_seq
@@ -146,85 +146,89 @@ CREATE SEQUENCE course_prerequisites_id_seq
     CACHE 1;
 
 
-ALTER TABLE course_prerequisites_id_seq OWNER TO dxucgijykcoqlp;
+ALTER TABLE course_prerequisites_id_seq OWNER TO teststudent;
 
 --
--- TOC entry 3048 (class 0 OID 0)
--- Dependencies: 187
--- Name: course_prerequisites_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: dxucgijykcoqlp
+-- TOC entry 2161 (class 0 OID 0)
+-- Dependencies: 190
+-- Name: course_prerequisites_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: teststudent
 --
 
 ALTER SEQUENCE course_prerequisites_id_seq OWNED BY course_prerequisites.id;
 
 
 --
--- TOC entry 2902 (class 2604 OID 2444096)
--- Name: course id; Type: DEFAULT; Schema: public; Owner: dxucgijykcoqlp
+-- TOC entry 2016 (class 2604 OID 16591)
+-- Name: course id; Type: DEFAULT; Schema: public; Owner: teststudent
 --
 
 ALTER TABLE ONLY course ALTER COLUMN id SET DEFAULT nextval('course_id_seq'::regclass);
 
 
 --
--- TOC entry 2904 (class 2604 OID 2444115)
--- Name: course_concurrents id; Type: DEFAULT; Schema: public; Owner: dxucgijykcoqlp
+-- TOC entry 2017 (class 2604 OID 16592)
+-- Name: course_concurrents id; Type: DEFAULT; Schema: public; Owner: teststudent
 --
 
 ALTER TABLE ONLY course_concurrents ALTER COLUMN id SET DEFAULT nextval('course_concurrents_id_seq'::regclass);
 
 
 --
--- TOC entry 2903 (class 2604 OID 2444106)
--- Name: course_prerequisites id; Type: DEFAULT; Schema: public; Owner: dxucgijykcoqlp
+-- TOC entry 2018 (class 2604 OID 16593)
+-- Name: course_prerequisites id; Type: DEFAULT; Schema: public; Owner: teststudent
 --
 
 ALTER TABLE ONLY course_prerequisites ALTER COLUMN id SET DEFAULT nextval('course_prerequisites_id_seq'::regclass);
 
 
 --
--- TOC entry 3030 (class 0 OID 2444088)
+-- TOC entry 2144 (class 0 OID 16576)
 -- Dependencies: 185
--- Data for Name: course; Type: TABLE DATA; Schema: public; Owner: dxucgijykcoqlp
+-- Data for Name: course; Type: TABLE DATA; Schema: public; Owner: teststudent
 --
 
 COPY course (id, name, units, summer, fall, winter, spring) FROM stdin;
-10	My Course	3	f	f	f	f
 11	My Other Course	3	f	f	f	f
+12	My New Course	3	f	f	f	f
+16	CMPS123	5	t	t	f	t
+17	My Test Course	1	f	f	f	f
+10	My Course	2.5	f	f	f	f
 \.
 
 
 --
--- TOC entry 3035 (class 0 OID 2444112)
--- Dependencies: 190
--- Data for Name: course_concurrents; Type: TABLE DATA; Schema: public; Owner: dxucgijykcoqlp
+-- TOC entry 2145 (class 0 OID 16579)
+-- Dependencies: 186
+-- Data for Name: course_concurrents; Type: TABLE DATA; Schema: public; Owner: teststudent
 --
 
 COPY course_concurrents (id, from_id, to_id) FROM stdin;
+14	16	12
 \.
 
 
 --
--- TOC entry 3049 (class 0 OID 0)
--- Dependencies: 189
--- Name: course_concurrents_id_seq; Type: SEQUENCE SET; Schema: public; Owner: dxucgijykcoqlp
+-- TOC entry 2162 (class 0 OID 0)
+-- Dependencies: 187
+-- Name: course_concurrents_id_seq; Type: SEQUENCE SET; Schema: public; Owner: teststudent
 --
 
-SELECT pg_catalog.setval('course_concurrents_id_seq', 1, false);
-
-
---
--- TOC entry 3050 (class 0 OID 0)
--- Dependencies: 186
--- Name: course_id_seq; Type: SEQUENCE SET; Schema: public; Owner: dxucgijykcoqlp
---
-
-SELECT pg_catalog.setval('course_id_seq', 11, true);
+SELECT pg_catalog.setval('course_concurrents_id_seq', 14, true);
 
 
 --
--- TOC entry 3033 (class 0 OID 2444103)
+-- TOC entry 2163 (class 0 OID 0)
 -- Dependencies: 188
--- Data for Name: course_prerequisites; Type: TABLE DATA; Schema: public; Owner: dxucgijykcoqlp
+-- Name: course_id_seq; Type: SEQUENCE SET; Schema: public; Owner: teststudent
+--
+
+SELECT pg_catalog.setval('course_id_seq', 17, true);
+
+
+--
+-- TOC entry 2148 (class 0 OID 16586)
+-- Dependencies: 189
+-- Data for Name: course_prerequisites; Type: TABLE DATA; Schema: public; Owner: teststudent
 --
 
 COPY course_prerequisites (from_id, to_id, id) FROM stdin;
@@ -232,17 +236,17 @@ COPY course_prerequisites (from_id, to_id, id) FROM stdin;
 
 
 --
--- TOC entry 3051 (class 0 OID 0)
--- Dependencies: 187
--- Name: course_prerequisites_id_seq; Type: SEQUENCE SET; Schema: public; Owner: dxucgijykcoqlp
+-- TOC entry 2164 (class 0 OID 0)
+-- Dependencies: 190
+-- Name: course_prerequisites_id_seq; Type: SEQUENCE SET; Schema: public; Owner: teststudent
 --
 
-SELECT pg_catalog.setval('course_prerequisites_id_seq', 1, false);
+SELECT pg_catalog.setval('course_prerequisites_id_seq', 1, true);
 
 
 --
--- TOC entry 2912 (class 2606 OID 2444117)
--- Name: course_concurrents course_concurrents_pkey; Type: CONSTRAINT; Schema: public; Owner: dxucgijykcoqlp
+-- TOC entry 2024 (class 2606 OID 16595)
+-- Name: course_concurrents course_concurrents_pkey; Type: CONSTRAINT; Schema: public; Owner: teststudent
 --
 
 ALTER TABLE ONLY course_concurrents
@@ -250,8 +254,8 @@ ALTER TABLE ONLY course_concurrents
 
 
 --
--- TOC entry 2906 (class 2606 OID 2444098)
--- Name: course course_pkey; Type: CONSTRAINT; Schema: public; Owner: dxucgijykcoqlp
+-- TOC entry 2020 (class 2606 OID 16597)
+-- Name: course course_pkey; Type: CONSTRAINT; Schema: public; Owner: teststudent
 --
 
 ALTER TABLE ONLY course
@@ -259,8 +263,8 @@ ALTER TABLE ONLY course
 
 
 --
--- TOC entry 2910 (class 2606 OID 2444109)
--- Name: course_prerequisites course_prerequisites_pkey; Type: CONSTRAINT; Schema: public; Owner: dxucgijykcoqlp
+-- TOC entry 2026 (class 2606 OID 16599)
+-- Name: course_prerequisites course_prerequisites_pkey; Type: CONSTRAINT; Schema: public; Owner: teststudent
 --
 
 ALTER TABLE ONLY course_prerequisites
@@ -268,8 +272,8 @@ ALTER TABLE ONLY course_prerequisites
 
 
 --
--- TOC entry 2908 (class 2606 OID 2444100)
--- Name: course coursename_unique; Type: CONSTRAINT; Schema: public; Owner: dxucgijykcoqlp
+-- TOC entry 2022 (class 2606 OID 16601)
+-- Name: course coursename_unique; Type: CONSTRAINT; Schema: public; Owner: teststudent
 --
 
 ALTER TABLE ONLY course
@@ -277,36 +281,24 @@ ALTER TABLE ONLY course
 
 
 --
--- TOC entry 3042 (class 0 OID 0)
--- Dependencies: 7
--- Name: public; Type: ACL; Schema: -; Owner: dxucgijykcoqlp
+-- TOC entry 2156 (class 0 OID 0)
+-- Dependencies: 3
+-- Name: public; Type: ACL; Schema: -; Owner: postgres
 --
 
 REVOKE ALL ON SCHEMA public FROM postgres;
-REVOKE ALL ON SCHEMA public FROM PUBLIC;
-GRANT ALL ON SCHEMA public TO dxucgijykcoqlp;
-GRANT ALL ON SCHEMA public TO PUBLIC;
 
 
 --
--- TOC entry 3044 (class 0 OID 0)
--- Dependencies: 575
--- Name: plpgsql; Type: ACL; Schema: -; Owner: postgres
---
-
-GRANT ALL ON LANGUAGE plpgsql TO dxucgijykcoqlp;
-
-
---
--- TOC entry 3045 (class 0 OID 0)
+-- TOC entry 2158 (class 0 OID 0)
 -- Dependencies: 185
--- Name: course; Type: ACL; Schema: public; Owner: dxucgijykcoqlp
+-- Name: course; Type: ACL; Schema: public; Owner: teststudent
 --
 
 GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE course TO PUBLIC;
 
 
--- Completed on 2017-03-06 17:29:55
+-- Completed on 2017-03-08 13:55:02 PST
 
 --
 -- PostgreSQL database dump complete
