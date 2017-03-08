9db7334bfcf7704be20ce06826cbb504f3f8578d
jinyingwu <jwu86@ucsc.edu>
2017-03-07 20:34:10 -0800

    Add files via upload

---
+++ CourseController.php
@@ -0,0 +1,137 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+/**
+ * Course Controller
+ *
+ * @property \App\Model\Table\CourseTable $Course
+ */
+class CourseController extends AppController
+{
+
+    /**
+     * Index method
+     *
+     * @return \Cake\Network\Response|null
+     */
+    public function index()
+    {
+        $course = $this->paginate($this->Course->find()->contain(['Concurrents', 'Prerequisites']));
+
+        $this->set(compact('course'));
+        $this->set('_serialize', ['course']);
+    }
+
+    /**
+     * View method
+     *
+     * @param string|null $id Course id.
+     * @return \Cake\Network\Response|null
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function view($id = null)
+    {
+        $course = $this->Course->get($id, [
+            'contain' => ['Concurrents', 'Prerequisites']
+        ]);
+
+        $this->set('course', $course);
+        $this->set('_serialize', ['course']);
+    }
+
+    /**
+     * Add method
+     *
+     * @return \Cake\Network\Response|null Redirects on successful add, renders view otherwise.
+     */
+    public function add()
+    {
+        $course = $this->Course->newEntity();
+        $this->set('coursenames', $this->Course->find('list'));
+        if ($this->request->is('post')) {
+            //var_dump($this->request->data);die();
+            $course = $this->Course->patchEntity($course, $this->request->data);
+            if($course->units >= 0) {
+                if ($result=$this->Course->save($course)) {
+                    $this->Flash->success(__('The course has been saved.'));
+                    $this->Course->saveConcurrents($result->id, $this->request->data["concurrents"]);
+                    $this->Course->savePrerequisites($result->id, $this->request->data["prerequisites"]);
+                    return $this->redirect(['action' => 'index']);
+                 }
+            } else {
+                $this->Flash->error(__('The units can not be negative.'));
+            }
+            $this->Flash->error(__('The course could not be saved. Please, try again.'));
+        }
+        $this->set(compact('course'));
+        $this->set('_serialize', ['course']);
+    }
+
+    /**
+     * Edit method
+     *
+     * @param string|null $id Course id.
+     * @return \Cake\Network\Response|null Redirects on successful edit, renders view otherwise.
+     * @throws \Cake\Network\Exception\NotFoundException When record not found.
+     */
+    public function edit($id = null)
+    {
+        $course = $this->Course->get($id, [
+            'contain' => ['Prerequisites', 'Concurrents']
+        ]);
+        $courseconcurrents = [];
+        foreach ($course['concurrents'] as $concurrent) {
+            array_push($courseconcurrents, $concurrent->id);
+        }
+        $courseprerequisites = [];
+        foreach ($course['prerequisites'] as $prerequisite) {
+            array_push($courseprerequisites, $prerequisite->id);
+        }
+        $this->set('coursenames', $this->Course->find('list'));
+        $this->set('courseprerequisites', $courseprerequisites);
+        $this->set('courseconcurrents', $courseconcurrents);
+        if ($this->request->is(['patch', 'post', 'put'])) {
+            $course = $this->Course->patchEntity($course, $this->request->data);
+            if($course->units >= 0) {
+                 if ($this->Course->save($course)) {
+                    $this->Course->deleteAssociations($id);
+                    $courseconcurrents = $this->request->data["concurrents"];
+                    $courseprerequisites = $this->request->data["prerequisites"];
+                    if (is_array($courseconcurrents)) $this->Course->saveConcurrents($id, $courseconcurrents);
+                    if (is_array($courseprerequisites)) $this->Course->savePrerequisites($id, $courseprerequisites);
+                        $this->Flash->success(__('The course has been saved.'));
+
+                    return $this->redirect(['action' => 'index']);
+                }
+            } else {
+                $this->Flash->error(__('The units can not be negative.'));
+            }
+            $this->Flash->error(__('The course could not be saved. Please, try again.'));
+        }
+        $this->set(compact('course'));
+        $this->set('_serialize', ['course']);
+    }
+
+    /**
+     * Delete method
+     *
+     * @param string|null $id Course id.
+     * @return \Cake\Network\Response|null Redirects to index.
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function delete($id = null)
+    {
+        $this->request->allowMethod(['post', 'delete']);
+        $course = $this->Course->get($id);
+        if ($this->Course->delete($course)) {
+            $this->Course->deleteAssociations($id);
+            $this->Flash->success(__('The course has been deleted.'));
+        } else {
+            $this->Flash->error(__('The course could not be deleted. Please, try again.'));
+        }
+
+        return $this->redirect(['action' => 'index']);
+    }
+}
