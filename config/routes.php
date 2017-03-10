83772be67cc97d049dc60436ae4ec9d39bef0396
James Huang <jameshuanguc@gmail.com>
2017-03-09 17:26:37 -0800

    redirect homepage to /course

--- config/routes.php
+++ config/routes.php
@@ -49,14 +49,14 @@ Router::scope('/', function (RouteBuilder $routes) {
      * its action called 'display', and we pass a param to select the view file
      * to use (in this case, src/Template/Pages/home.ctp)...
      */
-    $routes->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
+    // $routes->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
 
     /**
      * ...and connect the rest of 'Pages' controller's URLs.
      */
     $routes->connect('/pages/*', ['controller' => 'Pages', 'action' => 'display']);
 
-    $routes->redirect('/','http://tutorialspoint.com/');
+    $routes->redirect('/','https://cmps183.herokuapp.com/course');
 
     /**
      * Connect catchall routes for all controllers.
